a4f82f2eb32c764eaaceba07edb1dc6aad7a3a77
Alex <alexjooho@gmail.com>
2022-09-20 15:10:18 -0700

    finished exercise and further study

--- recursion.js
+++ recursion.js
@@ -18,7 +18,8 @@ function longest(words) {
 function everyOther(str) {
   if (str.length === 0) return '';
 
-  return str[0] += everyOther(str.slice(2));
+  return str[0] + everyOther(str.slice(2));
+  // use + instead of +=
 }
 
 /** find: return boolean depending on if val exists in array or not. */
@@ -26,15 +27,24 @@ function everyOther(str) {
 function find(arr, val) {
   if (arr.length === 0) return false;
 
-  return arr[0] === val || find(arr.slice(1), val);
+  // return arr[0] === val || find(arr.slice(1), val);  Previous solution
+  
+  if (arr[0] === val) return true;
+
+  return find(arr.slice(1), val);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
+  // if (str.length === 0) return true;
+  // let boolean = str[0] === str[str.length - 1];
+  // return boolean && isPalindrome(str.slice(1, -1));
+  
   if (str.length === 0) return true;
-  let boolean = str[0] === str[str.length - 1];
-  return boolean && isPalindrome(str.slice(1, -1));
+  if (str[0] !== str[str.length - 1]) return false;
+  
+  return isPalindrome(str.slice(1, -1));
 }
 
 /** revString: return a copy of a string, but in reverse. */
@@ -47,14 +57,33 @@ function revString(str) {
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {
-
+function findIndex(arr, val, index=0) {
+  if (arr.length === 0) return -1;
+  
+  if (arr[0] === val) return index;
+  
+  return findIndex(arr.slice(1), val, index +1 )
+  
+  // other possible solution:
+  // if (words.length === 0) return 0;
+  // const thisLength = words[0].length;
+  // return Math.max(thisLength, longest(words.slice(1)));
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
+function gatherStrings(obj, arr = []) {
+  for(let key in obj) {
+    if(typeof obj[key] === 'string') {
+      arr.push(obj[key])
+    }
+    
+    else if(typeof obj[key] === 'object') {
+      gatherStrings(obj[key], arr);
+    }
+  }
+  
+  return arr;
 }
 
 // FURTHER STUDY
@@ -62,17 +91,49 @@ function gatherStrings(obj) {
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
-function binarySearch(arr, val) {
+function binarySearch(arr, val, left = 0, right = arr.length) {
+  // If val is greater than arr[Math.floor(arr.length / 2)] then we start recursion in upper half
+  // else start recursion in lower half
+  
+  const upperHalf = Math.floor((left + right) / 2);
+  
+  if(val === arr[upperHalf]) return true;
+  
+  if(right - left <= 1) return false;
+  
+  if(val > arr[upperHalf]) {
+    return binarySearch(arr, val, upperHalf, right);
+  }
+  
+  else {
+    return binarySearch(arr, val, left, right - upperHalf - 1);
+  }
 
 }
 
 
-/** binarySearch: given a sorted array of numbers, and a value,
+/** binarySearchIndex: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearchIndex(arr, val) {
-
+function binarySearchIndex(arr, val, left = 0, right = arr.length) {
+  
+  const upperHalf = Math.floor((left + right) / 2);
+  
+  if(val === arr[upperHalf]) return upperHalf;
+  
+  if(right - left <= 1) return -1;
+  
+  if(val > arr[upperHalf]) {
+    return binarySearchIndex(arr, val, upperHalf, right);
+  }
+  
+  else {
+    return binarySearchIndex(arr, val, left, right - upperHalf - 1);
+  }
 }
+
+// We started off with using .slice, but we then added left and right parameters
+// so we don't have to keep making new arrays
 
 // you might find the above two problems easier if you change the function signature to:
 //
