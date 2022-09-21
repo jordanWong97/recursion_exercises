87bc6e80dc404ae30333b365ef9315435d3dbe85
Alex <alexjooho@gmail.com>
2022-09-20 17:47:11 -0700

    worked up to task 3 on splitsquares

--- splitSquares.js
+++ splitSquares.js
@@ -42,20 +42,42 @@ function validate(squares) {
 //simplify([1, 0, [1, [0, 0, 0, 0], 1, [1, 1, 1, 1]], 1])
 
 function simplify(squares) {
-  //array.every(i => i===1 || i ===0)
-  //if array, if square.every is square[0] return square[0]
-  for (let square of squares) {
+  // Try to go to the last most square and simplify it if possible
+  // Then work backwards and simplify every other square that can be simplified
+  // check if an array has all of the same values
 
+  if (!Array.isArray(squares)) return squares;
 
+  function _simplify(squares) {
+    // for(let square of squares) {
 
-    if (Array.isArray(square)) {
-      if (!square.every(i => i === square[0])) {
-        return false;
-      } else {
-        return square[0];
+    for (let miniSquare of squares) {
+      if (Array.isArray(miniSquare)) {
+        _simplify(miniSquare)
       }
     }
-    //simplify(square);
-  }
+
+    if (squares.every(i => i === squares[0])) {
+        console.log('3', squares, squares[0])
+        squares = squares[0];
+      
+        console.log('gets here 2', squares);
+      }
+      // const smallerSquareIndex = squares.findLastIndex(square => Array.isArray(square));
+
+    }
+  _simplify(squares);
+
+  // console.log('updated', squares);
+
   return squares;
 }
+
+// if(smallerSquareIndex !== -1) {
+//   console.log('gets here', squares[smallerSquareIndex]);
+//   _simplify(squares[smallerSquareIndex]);
+
+// if(squares.every(square => square === squares[0])) {
+//   squares = squares[0];
+//   console.log('gets here 2', squares)
+// }
